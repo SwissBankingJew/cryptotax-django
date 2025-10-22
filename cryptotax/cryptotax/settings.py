92991157cb707a26511fb28fa4557165b87e3ba5
Autistic Rambam Enjoyer <swissbankingjew@proton.me>
2025-10-22 13:54:33 +0000

    prod changes idk"

--- cryptotax/cryptotax/settings.py
+++ cryptotax/cryptotax/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Project-level static directory for assets like css/app.css
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
